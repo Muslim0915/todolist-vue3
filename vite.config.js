04451ce381658f5ef0653a7739c27c7213dd9d50
Muslim0915 <muslimpro0915@gmail.com>
2024-03-14 19:58:09 +0500

    adding changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/todolist-vue3/',
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
